adc2c9743b26564f60c0855556da76588566dd61
Alexandre <alexmondaini@gmail.com>
2020-12-06 21:46:09 +0800

    gatk-4-germilne commit

---
+++ gatk4-germline-snps-indels/haplotypecaller.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -N job_haplotypecaller
+#PBS -l walltime=40:00:00
+#PBS -l select=1:ncpus=2:mem=4gb
+#PBS -q cgsd
+module load java
+java -Dconfig.file=/groups/cgsd/$USER/gatk-workflows/my.conf \
+-jar /groups/cgsd/$USER/cromwell-53.1.jar run \
+/groups/cgsd/$USER/gatk-workflows/gatk4-germline-snps-indels/haplotypecaller-gvcf-gatk4.wdl \
+--inputs \
+/groups/cgsd/$USER/gatk-workflows/gatk4-germline-snps-indels/haplotypecaller-gvcf-gatk4.hg38.wgs.inputs.json
