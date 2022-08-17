aeb482d4adad4bf54bbb8e27233492660b568b36
Alexandre <alexmondaini@gmail.com>
2022-08-17 13:19:37 +0800

    liver workflow update

--- preprocess_fastq_to_bam/create-json.py
+++ preprocess_fastq_to_bam/create-json.py
@@ -1,7 +1,5 @@
-from pprint import pprint
 from pathlib import Path
 import json
-from io import StringIO 
 
 p = Path('/groups','cgsd','alexandre','cromwell-executions','ConvertPairedFastQsToUnmappedBamWf','d8e57ecb-a44d-4d7e-bc17-2a8acaacb7df','call-PairedFastQsToUnmappedBAM')
 
@@ -12,7 +10,7 @@ samples = sorted(p.glob('shard-*/execution/*.unmapped.bam'))
 samples = [x for x in samples if x.name.split('.')[0] not in done_samples]
 
 samples_names = [x.name.split('.')[0] for x in samples if x.name.split('.')[0] not in done_samples]
-
+print(len(samples_names))
 
 def create_data():
     data = []
