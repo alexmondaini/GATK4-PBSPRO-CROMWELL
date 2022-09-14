bfb0456b925e47960fb24594daa80318823ebe2a
Alexandre <alexmondaini@gmail.com>
2022-09-14 20:41:52 +0800

    c ________

--- hello_world/hello_world.bash
+++ hello_world/hello_world.bash
@@ -3,10 +3,11 @@
 #PBS -l walltime=4:00:00
 #PBS -l select=1:ncpus=2:mem=4gb
 #PBS -q cgsd
+#PBS -k oed
 
 module load java/11.0.9
 
-
-java -Dconfig.file=/groups/cgsd/$USER/gatk-workflows/application.conf \
--jar /groups/cgsd/$USER/cromwell-83.jar \
-run /groups/cgsd/$USER/gatk-workflows/hello_world/hello.wdl
+java -Dconfig.file=/groups/cgsd/alexandre/GATK_workflows/test.conf \
+-jar /groups/cgsd/alexandre/cromwell-83.jar \
+run /groups/cgsd/alexandre/GATK_workflows/hello_world/hello_world.wdl \
+--inputs /groups/cgsd/alexandre/GATK_workflows/hello_world/hello_world.json
